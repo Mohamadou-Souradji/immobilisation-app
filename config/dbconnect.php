677266006ad2a8003ca927b688e50a65196deee9
Souradji <souradjimohamadou@gmail.com>
2025-09-19 14:53:09 +0100

    Update dbconnect.php

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,12 +1,14 @@
 <?php
-$host = "sql310.infinityfree.com"; // fourni par InfinityFree
-$user = "if0_39894824";   // ton utilisateur MySQL
-$pass = "";
-$db   = "if0_39894824_gestion_immobilisation"; // ta base MySQL
+$host = "dpg-d301smfdiees738sr6h0-a";      // ex: dpg-xxxxx.frankfurt.render.com
+$port = "5432";
+$dbname = "immobilisation_db";    // ex: immobilisation_db
+$user = "immobilisation_db_user";             // ex: immobilisation_user
+$password = "lp2KNKsCXlpJyWjhIiQfbTeokTmFgtPW";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+// Connexion PostgreSQL
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
 if (!$conn) {
-    die("Connexion échouée : " . mysqli_connect_error());
+    die("Erreur de connexion à PostgreSQL : " . pg_last_error());
 }
 ?>
